9b70fa12ffbd08d152e41d7bdc6ebe875301def3
ll-eleanor <bobilyyi@gmail.com>
2024-10-20 22:17:12 -0400

    Basic Object Detection

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
 import cv2
+import os
+import supervision as sv
+
+from ultralytics import YOLO
+model = YOLO('yolov8n.pt')
+
+bounding_box_annotator = sv.BoundingBoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Unable to read camera feed")
+
+output_dir = 'output_images'
+os.makedirs(output_dir, exist_ok=True)
+
+img_counter = 0
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    results = model(frame)[0]
+    detections = sv.Detections.from_ultralytics(results)
+
+    annotated_image = bounding_box_annotator.annotate(scene=frame, detections=detections)
+    annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+
+    cv2.imshow('Webcam', annotated_image)
+
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        print("Escape hit, closing...")
+        break
+    elif k%256 == ord('s'):
+        img_name = os.path.join(output_dir, "opencv_frame_{}.png".format(img_counter))
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cap.release()
+cv2.destroyAllWindows
